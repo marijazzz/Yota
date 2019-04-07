b427e4c4b0f87719dd4ee55a5b4f9423a742f828
marijazzz <36822194+marijazzz@users.noreply.github.com>
2019-04-07 21:26:23 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -12,15 +12,21 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(954, 598)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pravila = QtWidgets.QPushButton(self.centralwidget)
-        self.pravila.setGeometry(QtCore.QRect(280, 290, 92, 36))
-        self.pravila.setObjectName("pravila")
+        self.rules = QtWidgets.QPushButton(self.centralwidget)
+        self.rules.setGeometry(QtCore.QRect(410, 260, 92, 36))
+        self.rules.setObjectName("rules")
+        self.start_game = QtWidgets.QPushButton(self.centralwidget)
+        self.start_game.setGeometry(QtCore.QRect(410, 210, 92, 36))
+        self.start_game.setObjectName("start_game")
+        self.drawButton = QtWidgets.QPushButton(self.centralwidget)
+        self.drawButton.setGeometry(QtCore.QRect(410, 160, 92, 36))
+        self.drawButton.setObjectName("drawButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 28))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 954, 28))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -33,6 +39,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pravila.setText(_translate("MainWindow", "правила"))
+        self.rules.setText(_translate("MainWindow", "Правила"))
+        self.start_game.setText(_translate("MainWindow", "Играть"))
+        self.drawButton.setText(_translate("MainWindow", "draw"))
 
 
--- window.py
+++ window.py
@@ -3,30 +3,51 @@ from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import start
+from PyQt5.QtGui import QPainter, QColor
 
 
 class StartDesigner(QMainWindow, start.Ui_MainWindow):
     def __init__(self, parent = None):
         super().__init__(parent = parent)
         self.setupUi(self)
-        self.pravila.clicked.connect(self.showMessage)
+        self.flag = False
+        self.rules.clicked.connect(self.showMessage)
+        self.start_game.clicked.connect(self.startGame)
+        self.drawButton.hide()
+        self.drawButton.clicked.connect(self.draw)
 
-
-    def quit(self):
-        QApplication.instance().quit()
 
     def showMessage(self):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
-
         msg.setText("Правила")
         msg.setInformativeText("This is additional information")
-
         msg.setStandardButtons(QMessageBox.Ok)
+        retval = msg.exec_()
 
+    def startGame(self):
+        self.start_game.hide()
+        self.rules.hide()
+        self.drawButton.show()
 
-        retval = msg.exec_() 
+    def draw(self):
+        self.flag = True
+        self.update()
 
+    def paintEvent(self, QPaintEvent):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.card(qp)
+            qp.end()
+
+    def card(self, qp):
+        qp.setBrush(QColor(255, 255, 255))
+        qp.drawRect(100, 100, 100, 100)
+        qp.setBrush(QColor(0, 0, 0))
+        qp.drawRect(120, 120, 60, 60)
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawEllipse(120 , 120 , 60, 60)
 
 if __name__ == '__main__':
     app = QApplication([])
