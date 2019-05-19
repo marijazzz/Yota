6e025704ec4a45c82046aa5b911de10b6e6a8f4c
NoValeria <37779234+NoValeria@users.noreply.github.com>
2019-05-19 22:05:31 +0300

    Add files via upload

--- Cards.py
+++ Cards.py
@@ -1,5 +1,5 @@
 class Card():
-    """класс, описывающий характеристики обычных карт"""
+    """класс, описывающий характеристики карт"""
 
     PROPERTY_NUM = 3
 
@@ -19,11 +19,37 @@ class Card():
     def __eq__(self, other):
         return self.properties == other.properties
 
+    # '''
+    # color identification (id)
+    # 1: Red
+    # 2: Blue
+    # 3: Green
+    # 4: Yellow
+    # '''
+
     def color(self):
         return self.color
 
+
+    # '''
+    # Value identification
+    # 1: 1
+    # 2: 2
+    # 3: 3
+    # 4: 4
+    # '''
+
     def value(self):
         return self.value
+
+
+    # '''
+    # Form identification (iden)
+    # 1: Triangle
+    # 2: Cross
+    # 3: Round
+    # 4: Square
+    # '''
 
     def form(self):
         return self.form
@@ -31,90 +57,3 @@ class Card():
 class RegularCard(Card):
     pass
 
-class Joker(Card):
-    """класс, описывающий карту Джокера"""
-
-    def __init__(self):
-        super().__init__(5,5,5)
-
-    def change(self, color, value, form):
-        """
-        Функция вызывается, когда игрок хочет поставить Джокера на поле и задает его параметры
-        """
-        self.color = color
-        self.value = value
-        self.form = form
-
-    def clear(self):
-        """
-        Функция вызывается, когда игрок забирает Джокера в руку
-        """
-        self.color = 5
-        self.value = 5
-        self.form = 5
-
-
-        # '''
-        # color identification (id)
-        # 1: Red
-        # 2: Blue
-        # 3: Green
-        # 4: Yellow
-        # '''
-        #
-        # class color:
-        #     """Класс, описывающий цвета карт"""
-        #     def __init__(self, id):
-        #         self.id = id
-        #
-        #     def __eq__(self, other):
-        #         """Переопределение равенства"""
-        #         return self.id == other.id
-        #
-        #     def __ne__(self, other):
-        #         """Переопределение неравенства"""
-        #         return not (self.id == other.id)
-        #
-        #
-        # '''
-        # Value identification
-        # 1: 1
-        # 2: 2
-        # 3: 3
-        # 4: 4
-        # '''
-        #
-        # class Value:
-        #     """Класс, описывающий численное значение карт"""
-        #     def __init__(self, value):
-        #         self.value = value
-        #
-        #     def __eq__(self, other):
-        #         """Переопределение равенства"""
-        #         return self.value == other.value
-        #
-        #     def __ne__(self, other):
-        #         """Переопределение неравенства"""
-        #         return not (self.value == other.value)
-        #
-        #
-        # '''
-        # Form identification (iden)
-        # 1: Triangle
-        # 2: Cross
-        # 3: Round
-        # 4: Square
-        # '''
-        #
-        # class Form:
-        #     """Класс, описывающий форму карт"""
-        #     def __init__(self, iden):
-        #         self.iden = iden
-        #
-        #     def __eq__(self, other):
-        #         """Переопределение равенства"""
-        #         return self.iden == other.iden
-        #
-        #     def __ne__(self, other):
-        #         """Переопределение неравенства"""
-        #         return not (self.iden == other.iden)
